b85c0a23e5f2de5b51b32075a7a98731c1a98f5b
dglabs <reach.dglabs@gmail.com>
2020-06-27 19:49:02 +0530

    Deployment changes in package.json, added server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+app.use(forceSSL());
+
+// Serve static files....
+app.use(express.static(__dirname + '/dist'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
